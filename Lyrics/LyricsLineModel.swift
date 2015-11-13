afefc27eb505199263cc75e913fe03fbc02d170f
MichaelRow <Eru@macbook-pro.local>
2015-11-13 08:56:08 +0800

    fix setter bug which case circular references

--- Lyrics/LyricsLineModel.swift
+++ Lyrics/LyricsLineModel.swift
@@ -12,50 +12,45 @@ class LyricsLineModel: NSObject {
     
     var lyricsSentence:NSString!
     
-    var msecPosition:Int! {
-        set {
-            let minute:Int = newValue/(60*1000)
-            let second:Int = (newValue-minute*60*1000)/1000
-            let mSecond:Int = newValue-(minute*60+second)*1000
-            var theTimeTag:String = String()
-            
-            if minute < 10 {
-                theTimeTag += "[0\(minute):"
-            } else {
-                theTimeTag += "[\(minute):"
-            }
-            
-            if second < 10 {
-                theTimeTag += "0\(second)."
-            } else {
-                theTimeTag += "\(second)."
-            }
-            
-            if mSecond > 99 {
-                theTimeTag += "\(mSecond)]"
-            } else if mSecond > 9 {
-                theTimeTag += "0\(second)]"
-            } else {
-                theTimeTag += "00\(second)]"
-            }
-            self.timeTag = theTimeTag
-        }
-        get {
-            return self.msecPosition
-        }
+    var msecPosition:Int!
+    
+    var timeTag:NSString!
+    
+    func setMsecPositionWithTimeTag (theTimeTag: NSString) {
+        self.timeTag=theTimeTag
+        let colonRange:NSRange = theTimeTag.rangeOfString(":")
+        let minuteStr:NSString = theTimeTag.substringWithRange(NSMakeRange(1, colonRange.location-1))
+        let secondStr:NSString = theTimeTag.substringWithRange(NSMakeRange(colonRange.location+1, theTimeTag.length-colonRange.length-colonRange.location-1))
+        let minute:Int = minuteStr.integerValue
+        let second:Float = secondStr.floatValue
+        self.msecPosition = Int((Float(minute)*60+second)*1000)
     }
     
-    var timeTag:NSString! {
-        set {
-            let colonRange:NSRange = newValue.rangeOfString(":")
-            let minuteStr:NSString = newValue.substringWithRange(NSMakeRange(1, colonRange.location-1))
-            let secondStr:NSString = newValue.substringWithRange(NSMakeRange(colonRange.location+1, newValue.length-colonRange.length-colonRange.location-1))
-            let minute:Int = minuteStr.integerValue
-            let second:Float = secondStr.floatValue
-            self.msecPosition = Int((Float(minute)*60+second)*1000)
+    func setTimeTagWithMsecPosition (theMsecPosition: Int) {
+        let minute:Int = theMsecPosition/(60*1000)
+        let second:Int = (theMsecPosition-minute*60*1000)/1000
+        let mSecond:Int = theMsecPosition-(minute*60+second)*1000
+        var theTimeTag:String = String()
+        
+        if minute < 10 {
+            theTimeTag += "[0\(minute):"
+        } else {
+            theTimeTag += "[\(minute):"
         }
-        get {
-            return self.timeTag
+        
+        if second < 10 {
+            theTimeTag += "0\(second)."
+        } else {
+            theTimeTag += "\(second)."
         }
+        
+        if mSecond > 99 {
+            theTimeTag += "\(mSecond)]"
+        } else if mSecond > 9 {
+            theTimeTag += "0\(second)]"
+        } else {
+            theTimeTag += "00\(second)]"
+        }
+        self.timeTag = theTimeTag
     }
 }
